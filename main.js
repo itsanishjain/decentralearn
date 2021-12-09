5c56ae85ff335e5530ed7cf842e37dffbd4a29fa
anish jain <helloanishjain@gmail.com>
2021-12-10 00:30:48 +0530

    link pages and add metamask login

--- main.js
+++ main.js
@@ -1,85 +1,61 @@
-/* TODO: Add Moralis init code */
-
 /* Moralis init code */
 const serverUrl = "https://pwdgajlkixh3.usemoralis.com:2053/server";
 const appId = "Gws8paRYa2auAnWgLoQYCcSbdOpugC3vbGsMdEWW";
 Moralis.start({ serverUrl, appId });
-console.log("Moralis initiated");
 
-/* TODO: Add Moralis Authentication code */
+// Helper function
+hideElement = (element) => (element.style.display = "none");
+showElement = (element) => (element.style.display = "block");
+
+// get the navigation bar and logout button
+const navbar = document.getElementById("nav");
+const logoutBtn = document.getElementById("btn-logout");
+
+// call when page loads to check wheather user is logged in or not
+
+const init = () => {
+  console.log("Initiated");
+  let user = Moralis.User.current();
+  if (user) {
+    console.log(user, "alredy we have your data in our database");
+    document.getElementById("root").innerHTML =
+      "Logged in as " + user.get("ethAddress");
+    showElement(navbar);
+    showElement(logoutBtn);
+  } else {
+    console.log("NOT LOGGED IN");
+    hideElement(navbar);
+    hideElement(logoutBtn);
+  }
+};
 
 /* Authentication code */
 async function login() {
-    let user = Moralis.User.current();
-    console.log('USER IS HERE', user);
-    if (!user) {
-        user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
-            .then(function (user) {
-                console.log("logged in user:", user);
-                console.log(user.get("ethAddress"));
-
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-    console.log("logged in user:", user);
-    const options = { address: "0x41de50BB1Bd1A0Fecc0f04a747c0e36D6AaBd9f3", chain: "mumbai" };
-    const NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
-    console.log(NFTs);
-    parseMetadata(NFTs.result);
-
-    document.getElementById("root").innerHTML = user.get("ethAddress");
+  let user = Moralis.User.current();
+  console.log("USER IS HERE", user);
+  if (!user) {
+    user = await Moralis.authenticate({
+      signingMessage: "Log in using Moralis",
+    }).then(function (user) {
+      console.log("logged in user:", user);
+      console.log(user.get("ethAddress"));
+      document.getElementById("root").innerHTML =
+        "Logged in as " + user.get("ethAddress");
+    });
+    // showElement(navbar).catch(function (error) {
+    //   console.log(error);
+    // });
+    showElement(logoutBtn);
+  }
 }
-
-function parseMetadata(results) {
-    console.log(results);
-    for (let i = 0; i < results.length; i++) {
-        console.log(results[i])
-        let parsedNFTsMetadata = JSON.parse(results[i].metadata);
-
-        console.log(parsedNFTsMetadata);
-        console.log(parsedNFTsMetadata);
-        let img = new Image();
-        img.src = parsedNFTsMetadata.image;
-        img.width = "100";
-        img.height = "100";
-
-        console.log(img);
-
-        img.onload = function () {
-            console.log(img.width);
-            console.log(img.height);
-            let nftDiv = document.getElementById("image")
-            // .src = img.src;
-            nftDiv.appendChild(img);
-        }
-        console.log(img.src);
-
-
-    }
-
-
-}
-
 
 async function logOut() {
-    await Moralis.User.logOut();
-    window.location.reload();
-    console.log("logged out");
+  await Moralis.User.logOut();
+  window.location.reload();
+  console.log("logged out");
 }
 
-login()
 document.getElementById("btn-login").onclick = login;
-document.getElementById("btn-logout").onclick = logOut;
+logoutBtn.onclick = logOut;
 
-
-
-function hexpadded(HexId) {
-    let hexInt = parseInt(HexId).toString();
-    let padded = ("0000000000000000000000000000000000000000000000000000000000000000" + hexInt).slice(-64)
-    console.log(padded.toString().length);
-
-}
-
-// hexpadded(12)
+init();
